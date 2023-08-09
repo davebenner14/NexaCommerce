0094021d3806e6baca1315851f3f03f07575143d
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-09 17:09:16 -0400

    Update ProductFactory define data blueprint for products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
- */
 class ProductFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->productName,  // assuming there's a method like productName in Faker
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 1, 1000),  // 2 decimals, min 1, max 1000
+            'stock' => $this->faker->numberBetween(1, 100),
+            'image' => $this->faker->imageUrl(),  // example placeholder image URL
         ];
     }
 }
