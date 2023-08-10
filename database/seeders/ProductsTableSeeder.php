049875bf9a304a62b1d708d0b4eb22a325eb8c61
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-10 13:38:13 -0400

    Update ProductsTableSeeder to include 50 dummy products

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -2,16 +2,19 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        //
+        // Create 50 dummy products
+        Product::factory(50)->create();
     }
 }
