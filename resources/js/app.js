b538b17fab54d4d6a972b1d5294d74d8a74b4377
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-10 16:55:48 -0400

    Update Vite config, App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,19 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
+// AlpineJS
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
-
 Alpine.start();
+
+// VueJS
+import Vue from "vue";
+import ProductList from "./components/ProductList.vue";
+
+Vue.component("product-list", ProductList);
+
+const app = new Vue({
+    el: "#app",
+});
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue"; // Import the Vue plugin
 
 export default defineConfig({
     plugins: [
+        vue(), // Add the Vue plugin here
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
 });
