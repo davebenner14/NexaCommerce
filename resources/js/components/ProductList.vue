b1dab25d27afd0a607b3c5eb21de6d6af2a28ae6
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-10 16:26:42 -0400

    Create ProductList component

---
+++ resources/js/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-for="product in products" :key="product.id">
+            {{ product.name }} - {{ product.description }} - ${{
+                product.price
+            }}
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            products: [],
+        };
+    },
+    mounted() {
+        axios
+            .get("/api/products")
+            .then((response) => {
+                this.products = response.data;
+            })
+            .catch((error) => {
+                console.error("There was an error fetching products:", error);
+            });
+    },
+};
+</script>
