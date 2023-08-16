193b3ab88710ad9ae4294c23389c01ce64aa9aba
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-16 12:10:19 -0400

    Add Dashboard component with template

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Your Dashboard</h1>
+        <h2>Recent Purchases</h2>
+        <ul>
+            <li v-for="purchase in purchases" :key="purchase.id">
+                {{ purchase.productName }} - {{ purchase.date }}
+            </li>
+        </ul>
+    </div>
+</template>
