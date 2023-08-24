08aa1757c1a7d5626eefa10d56f50e34537d854c
David Benner <119465113+davebenner14@users.noreply.github.com>
2023-08-24 13:13:10 -0400

    Add Index Page to and set up all routes to direct to index page instead of default laravel dashbaord

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,36 +1,6 @@
 <template>
     <div>
-        <h1>Your Dashboard</h1>
-        <h2>Recent Purchases</h2>
-        <ul>
-            <li v-for="purchase in purchases" :key="purchase.id">
-                {{ purchase.productName }} - {{ purchase.date }}
-            </li>
-        </ul>
+        <h1>Hello from Dashboard</h1>
     </div>
 </template>
-<script>
-import axios from "axios";
-
-export default {
-    data() {
-        return {
-            purchases: [],
-        };
-    },
-    mounted() {
-        this.fetchRecentPurchases();
-    },
-    methods: {
-        async fetchRecentPurchases() {
-            try {
-                const response = await axios.get("/api/user/purchases");
-                this.purchases = response.data;
-            } catch (error) {
-                console.error("Error fetching recent purchases:", error);
-                // Handle the error accordingly
-            }
-        },
-    },
-};
-</script>
+created() { console.log("Dashboard component created"); }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Welcome to Index Page') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                Welcome to the index page for logged-in users!
+            </div>
+        </div>
+    </div>
+</x-app-layout>
